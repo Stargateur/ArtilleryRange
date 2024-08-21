3a43a0fe1b627ef4b9b1628b728d9b4472585d9c
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2024-08-21 22:39:48 +0200

    0.0.0

---
+++ data.lua
@@ -0,0 +1,4 @@
+data.raw["gun"]["artillery-wagon-cannon"].attack_parameters.range =
+    settings.startup["artillery-range"].value
+data.raw["gun"]["artillery-wagon-cannon"].attack_parameters.min_range =
+    settings.startup["artillery-range-min"].value
---
+++ settings.lua
@@ -0,0 +1,16 @@
+data:extend {
+  {
+    type = "int-setting",
+    name = "artillery-range",
+    setting_type = "startup",
+    default_value = 512,
+    minimum_value = 1,
+  },
+  {
+    type = "int-setting",
+    name = "artillery-range-min",
+    setting_type = "startup",
+    default_value = 64,
+    minimum_value = 1,
+  },
+}
